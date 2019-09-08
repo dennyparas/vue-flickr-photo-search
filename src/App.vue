944911bcc22a98d16907eea47d02e45af787ba6a
dennyparas <dennyparas28@gmail.com>
2019-09-08 17:16:51 +0800

    add TheNavbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <the-navbar />
     <router-view/>
   </div>
 </template>
+<script>
+import TheNavbar from '@/components/layout/TheNavbar'
+export default {
+  name: 'App',
+  components: {
+    TheNavbar
+  }
+
+}
+</script>
---
+++ src/components/layout/TheNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand class="mb-0 h1 mx-auto">Vue Flickr Photo Finder</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar'
+}
+</script>
+
+<style>
+
+</style>
